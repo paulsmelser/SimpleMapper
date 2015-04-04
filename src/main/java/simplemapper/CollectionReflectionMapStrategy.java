ead053b2e6f04efbf798945c4cf7e5a9a954c90a
Paul Smelser <paul.smelser@gmail.com>
2015-04-04 02:00:07 -0400

    fixing mapper problems

--- src/main/java/simplemapper/CollectionReflectionMapStrategy.java
+++ src/main/java/simplemapper/CollectionReflectionMapStrategy.java
@@ -1,34 +1,47 @@
 package simplemapper;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 
-class CollectionReflectionMapStrategy implements FieldMapStrategy{
+class CollectionReflectionMapStrategy implements FieldMapStrategy {
 
-	@SuppressWarnings({ "unchecked" })
-	@Override
-	public <R, T> R map(T from, R to, Method method) throws MapperException{
-		try {
-		Field it = to.getClass().getDeclaredField(method.getName().substring(3).toLowerCase());
-		ParameterizedType stringListType = (ParameterizedType) it.getGenericType();
-		Class<?> stringListClass = (Class<?>) stringListType.getActualTypeArguments()[0];
+    @SuppressWarnings({ "unchecked" })
+    @Override
+    public <R, T> R map(T from, R to, Method method) throws MapperException {
+	try {
+	    String property = method.getName().substring(3);
+	    Field it = to.getClass().getDeclaredField(property.toLowerCase());
+	    ParameterizedType stringListType = (ParameterizedType) it.getGenericType();
+	    Class<?> stringListClass = (Class<?>) stringListType.getActualTypeArguments()[0];
 
-		if(from instanceof Collection){
-			for(Object o :(Collection<?>)  from){
-				((Collection<Object>)to).add(Mapper.map(o, stringListClass));
-			}
+	    Method getMethod = to.getClass().getMethod(method.getName());
+
+	    if (getMethod.invoke(to) == null) {
+		to.getClass().getMethod("set" + property, getMethod.getReturnType())
+			.invoke(to, new ArrayList<>());
+	    }
+	    if (from instanceof Collection) {
+		for (Object o : (Collection<?>) from) {
+		    ((Collection<Object>) getMethod.invoke(to)).add(Mapper.map(o, stringListClass));
 		}
-		} catch (IllegalArgumentException e) {
-			throw new MapperException(e);
-		} catch (SecurityException e) {
-			throw new MapperException(e);
-		} catch (NoSuchFieldException e) {
-			throw new MapperException(e);
-		}
-		return to;
+	    }
+	} catch (IllegalArgumentException e) {
+	    throw new MapperException(e);
+	} catch (SecurityException e) {
+	    throw new MapperException(e);
+	} catch (NoSuchFieldException e) {
+	    throw new MapperException(e);
+	} catch (NoSuchMethodException e) {
+	    throw new MapperException(e);
+	} catch (IllegalAccessException e) {
+	    throw new MapperException(e);
+	} catch (InvocationTargetException e) {
+	    throw new MapperException(e);
 	}
-
+	return to;
+    }
 }
--- src/test/java/entities/BarComplexList.java
+++ src/test/java/entities/BarComplexList.java
@@ -1,16 +1,15 @@
 package entities;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class BarComplexList {
-	private List<Bar> list = new ArrayList<Bar>();
+    private List<Bar> list;
 
-	public List<Bar> getList() {
-		return list;
-	}
+    public List<Bar> getList() {
+	return list;
+    }
 
-	public void setList(List<Bar> list) {
-		this.list = list;
-	}
+    public void setList(List<Bar> list) {
+	this.list = list;
+    }
 }
